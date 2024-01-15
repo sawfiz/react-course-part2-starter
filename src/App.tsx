d7fb76fe4d251af1779e26dc19c23e1f720ab011
sawfiz <112825751+sawfiz@users.noreply.github.com>
2024-01-15 16:23:20 +0800

    Use useQuery to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import './App.css';
+import "./App.css";
+import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <h1>React Starter Project</h1>;
+  return (
+    <>
+      <h1>React Starter Project</h1>
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,5 +1,6 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+// import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 interface Todo {
   id: number;
@@ -9,21 +10,37 @@ interface Todo {
 }
 
 const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
+  const fetchTodos = () =>
     axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => setTodos(res.data))
-      .catch((error) => setError(error));
-  }, []);
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        console.log(res.data);
+        return res.data;
+      });
 
-  if (error) return <p>{error}</p>;
+  // Queries
+  // Call queryFn, deconstruct the data, rename to todos
+  // Save the data in cache under "todos"
+  const { data: todos } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
+
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  // const [error, setError] = useState('');
+
+  // useEffect(() => {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/todos')
+  //     .then((res) => setTodos(res.data))
+  //     .catch((error) => setError(error));
+  // }, []);
+
+  // if (error) return <p>{error}</p>;
 
   return (
     <ul className="list-group">
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <li key={todo.id} className="list-group-item">
           {todo.title}
         </li>
