c8ca9e8350bd6f5c0784cde423a2100e63dbb824
sawfiz <112825751+sawfiz@users.noreply.github.com>
2024-01-16 15:59:55 +0800

    Add optimistic update

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,51 +1,81 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { useRef } from "react";
-import useTodos, { Todo } from "./hooks/useTodos";
 import { queryClient } from "../main";
+import { Todo } from "./hooks/useTodos";
+
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
 
 const TodoForm = () => {
   const ref = useRef<HTMLInputElement>(null);
 
-  const addTodo = useMutation({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .post<Todo>("https://jsonplaceholder.typicode.com/todosx", todo)
         .then((res) => res.data),
+
+    onMutate: (newTodo: Todo) => {
+      // Save the original todo list
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+      // Optimisitcally clear the form on success
+      if (ref.current) ref.current.value = "";
+      // Add the new Todo to the list
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        newTodo,
+        ...(todos || []),
+      ]);
+      return { previousTodos };
+    },
+
     onSuccess: (savedTodo, newTodo) => {
       // Approach 1: Invalidating the cache
       // queryClient.invalidateQueries({ queryKey: ["todos"] });
 
       // Approach 2: Updating the data in the the cache
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        savedTodo,
-        ...(todos || []),
-      ]);
+      // If sucessful, replace newTod with savedTodo
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+
+    onError: (Error, newTodo, context) => {
+      if (!context) return;
+      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
     },
   });
 
   return (
-    <form
-      className="row mb-3"
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (ref.current && ref.current.value)
-          // call addTodo.mutate, the object is sent to mutationFn
-          addTodo.mutate({
-            id: 0,
-            title: ref.current?.value,
-            userId: 1,
-            completed: false,
-          });
-      }}
-    >
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+    <>
+      {addTodo.error && (
+        <div className=" alert alert-danger"> {addTodo.error.message}</div>
+      )}
+      <form
+        className="row mb-3"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (ref.current && ref.current.value)
+            // call addTodo.mutate, the object is sent to mutationFn
+            addTodo.mutate({
+              id: 0,
+              title: ref.current?.value,
+              userId: 1,
+              completed: false,
+            });
+        }}
+      >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button disabled={addTodo.isPending} className="btn btn-primary">
+            {addTodo.isPending ? "Saving" : "Add"}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
